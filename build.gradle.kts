e3cb9b361c6fef2248282ddc2071cb909a8a58e1
Edgar <2330584@upt.edu.mx>
2026-06-04 01:11:14 -0600

    Segundo commit al backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,33 @@
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(ktorLibs.plugins.ktor)
     alias(libs.plugins.kotlin.serialization)
+    application
 }
 
 group = "com.example"
 version = "1.0.0-SNAPSHOT"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass.set("com.example.MainKt")
 }
 
 kotlin {
     jvmToolchain(21)
 }
+
 dependencies {
     implementation(ktorLibs.server.config.yaml)
     implementation(ktorLibs.server.contentNegotiation)
     implementation(ktorLibs.server.core)
     implementation(ktorLibs.server.netty)
     implementation(ktorLibs.server.resources)
-    implementation(libs.logback.classic)
+
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+
+    implementation(libs.logback.classic)
+
     testImplementation(kotlin("test"))
     testImplementation(ktorLibs.server.testHost)
 }
